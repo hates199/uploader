e4dc02ad90d3442ee2796f764192f5a6229071ad
Olonlog Tuv <olonlogtuv@Olonlogs-MacBook-Air.local>
2019-07-10 00:05:18 +0800

    v1.0

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import os
 # Init & Config
 app = Flask(__name__)
 dropzone = Dropzone(app)
+# Session key
 app.config['SECRET_KEY'] = 'secret_key'
 
 # Dropzone settings
@@ -14,13 +15,16 @@ app.config['SECRET_KEY'] = 'secret_key'
 app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
 app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
 app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
+# Upload->(Redirect)->results.html(@app.route(results))
 app.config['DROPZONE_REDIRECT_VIEW'] = 'results'
 
 # Upload settings
-
 app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd() + '/uploads'
 
+# An “upload set” is a single collection of files. You just declare them in the code:
 photos = UploadSet('photos', IMAGES)
+# It will go through all the upload sets, 
+# get their configuration, and store the configuration on the app
 configure_uploads(app, photos)
 patch_request_class(app) # Max file size
 
@@ -29,6 +33,7 @@ patch_request_class(app) # Max file size
 # Routes for app
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
     #Sessions
     if "file_urls" not in session:
         session['file_urls'] = []
@@ -54,6 +59,7 @@ def results():
     file_urls = session['file_urls']
     session.pop('file_urls', None)
     return render_template('results.html', file_urls=file_urls)
+# Extra
 @app.route('/about')
 def about():
     return render_template('about.html')
